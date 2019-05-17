afa1434ae1f1fea651d4457f377a22aa25632165
WatsonJay <446725026@qq.com>
2019-05-17 17:33:47 +0800

    Update app.py

--- learning_flask/app.py
+++ learning_flask/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, url_for, render_template
+from flask_sqlalchemy import  SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:@106.14.220.77:3306/db_demo?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 name = 'Grey Li'
 movies = [
