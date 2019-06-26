44e9aeb3a4fa1b8814801fb007b11199405e63b0
WatsonJay <446725026@qq.com>
2019-06-26 17:43:24 +0800

    引入mysql

--- learning_flask/app.py
+++ learning_flask/app.py
@@ -1,9 +1,19 @@
 from flask import Flask, url_for, render_template
 from flask_sqlalchemy import  SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:@106.14.220.77:3306/db_demo?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://root:@127.0.0.1:3306/test?charset=utf8'
+
+#配置flask配置对象中键：SQLALCHEMY_COMMIT_TEARDOWN,设置为True,应用会自动在每次请求结束后提交数据库中变动
+
+app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+#获取SQLAlchemy实例对象，接下来就可以使用对象调用数据
+
+db = SQLAlchemy(app)
 
 name = 'Grey Li'
 movies = [
---
+++ learning_flask/models.py
@@ -0,0 +1,2 @@
+from app import db #db是在app/__init__.py生成的关联后的SQLAlchemy实例
+
